6ac7b57c481957a3a4a28dee44415b8e4d56979d
Leila Mahammed <devmldesign@gmail.com>
2024-04-16 09:11:00 +0200

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { AcceuilComponent } from "./acceuil/acceuil.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, RouterLink, RouterLinkActive]//ont a retireer le acceuilComponent pour le routeurLink
 })
 export class AppComponent {
   title = 'devweb2-demo';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { AcceuilComponent } from './acceuil/acceuil.component';
+import { PageNonTrouveComponent } from './page-non-trouve/page-non-trouve.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'acceuil' , component : AcceuilComponent},
+    {  path: 'connexion', component : ConnexionComponent},
+    {  path: '', redirectTo : 'acceuil', pathMatch : 'full'},//configuration doc angular
+    {  path: '**', component : PageNonTrouveComponent}//ajouter un templat pour la page non trouver n'import quels caractere nimporte quoi 
+
+];
